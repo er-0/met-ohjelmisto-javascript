5dc960be7694046fcd6271a57ab5de4104af531f
er-0 <95697943+er-0@users.noreply.github.com>
2025-11-04 18:04:00 +0200

    Exercises 6 & 7

---
+++ mod-01/teht-06/t6.js
@@ -0,0 +1,19 @@
+const confirmed = confirm('Should I calculate the square root?')
+let output
+
+if (confirmed) {
+  const number = +prompt('Give a number');
+
+  if (number < 0) {
+    output = `The square root of a negative number is not defined.`
+  }
+  else {
+    const sqrt = Math.sqrt(number)
+    output = `The square root of ${number} is ${sqrt}.`
+  }
+}
+else {
+  output = `The square root is not calculated.`
+}
+
+document.querySelector('#target').innerHTML = output
---
+++ mod-01/teht-07/t7.js
@@ -0,0 +1,15 @@
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+const throws = +prompt('How many rolls of the dice?')
+let results = 0
+
+for (let i = 0; i < throws; i++) {
+  results += getRandomIntInclusive(1, 6)
+  console.log(i, results)
+}
+
+document.querySelector('#target').innerHTML = `The sum of the rolls is ${results}.`
