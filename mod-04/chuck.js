ed480e89cb4aea7673e81e34225629b883a6205c
er-0 <95697943+er-0@users.noreply.github.com>
2025-11-14 19:53:19 +0200

    Module 4, Chuck Norris jokes second step (done)

--- mod-04/chuck.js
+++ mod-04/chuck.js
@@ -1,17 +1,29 @@
 'use strict';
 
-const url = `https://api.chucknorris.io/jokes/random`;
+const formElem = document.querySelector('#chuckForm');
 
-async function loadJoke() {
+formElem.addEventListener('submit', async function(evt) {
+  evt.preventDefault();
+  const input = formElem.querySelector('input[name=q]').value;
+  const url = `https://api.chucknorris.io/jokes/search?query=${input}`;
+
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.value);
-    /*const res = document.querySelector('#results');
-    res.innerHTML = data.value;*/
+    const jokeArray = data.result;
+    const res = document.querySelector('#results');
+    res.innerHTML = ``
+
+    for (let jokeElem of jokeArray) {
+      const p = document.createElement('p');
+      p.innerText = jokeElem.value;
+
+      const a = document.createElement('article');
+      a.append(p);
+
+      res.append(a);
+    }
   } catch (error) {
     console.log(error);
   }
-}
-
-void loadJoke();
+});
