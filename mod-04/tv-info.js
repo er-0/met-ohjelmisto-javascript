4e71bd718ff797f3f5dc3fface8eb56c29331656
er-0 <95697943+er-0@users.noreply.github.com>
2025-11-14 19:01:12 +0200

    Module 4, TV info page third step (done)

--- mod-04/tv-info.js
+++ mod-04/tv-info.js
@@ -3,14 +3,46 @@
 const tvFormElem = document.querySelector('#tvForm');
 
 tvFormElem.addEventListener('submit', async function(evt) {
-  evt.preventDefault()
+  evt.preventDefault();
   const input = tvFormElem.querySelector('input[name=q]').value;
   const url = `https://api.tvmaze.com/search/shows?q=${input}`;
+
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    const res = document.querySelector('#results');
+    res.innerHTML = ``;
+
+    if (data.length === 0) {
+      res.innerHTML = `<p>No results found.</p>`;
+    }
+
+    for (let element of data) {
+      const s = element.show;
+      const h = document.createElement('h2');
+      h.textContent = s.name;
+
+      const a = document.createElement('a');
+      a.href = s.url;
+      a.innerText = s.url;
+      a.target = '_blank';
+
+      const i = document.createElement('img');
+      i.src = (s.image && s.image.medium)
+          ? s.image.medium
+          : 'https://placehold.co/210x295?text=Not%20Found';
+      i.alt = s.name;
+
+      const d = document.createElement('div');
+      d.innerHTML = s.summary;
+
+      const article = document.createElement('article');
+      article.append(h, i, a, d);
+
+      res.append(article);
+    }
+
   } catch (error) {
     console.log(error);
   }
-})
+});
